6d30a1080b8626f4b37003cd89c81ed92393af22
manvithaindukuru <manvithaindukuru@gmail.com>
2026-09-03 16:27:31 +0530

    Added Students API

--- src/main/java/com/manvitha/StudentService/controller/TestController.java
+++ src/main/java/com/manvitha/StudentService/controller/TestController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/test")
@@ -15,5 +18,20 @@ public class TestController {
     @GetMapping("/hello")
     public ResponseEntity<?>hello(){
         return new ResponseEntity( "hello", HttpStatus.OK);
+    }
+    @GetMapping("/hi")
+    public ResponseEntity<?>hi(){
+        return new ResponseEntity( "hi", HttpStatus.OK);
+    }
+    @GetMapping("/students")
+    public ResponseEntity<?>getStudents(){
+        Map<String, Integer> students =new HashMap<>();
+        students.put("Hari", 22);
+        students.put("giri", 21);
+        students.put("pari", 23);
+        students.put("siri", 25);
+        students.put("nari", 24);
+        return new ResponseEntity<>(students, HttpStatus.OK);
+
     }
 }
